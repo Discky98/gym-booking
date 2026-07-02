79d53caf591e28404b44523c1215de9e3f53d85a
Bwire Boniphace <bwireboni98@gmail.com>
2026-07-02 07:36:08 +0300

    added messages on logout operation

--- pages/logout.php
+++ pages/logout.php
@@ -2,5 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
+$_SESSION['flash_message'] = ['type' => 'ok', 'text' => 'You have been logged out successfully.'];
 header("Location: ../login.php");
 exit;
